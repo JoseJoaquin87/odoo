1c1d61ea383027a6718dacd15ea6621a388ac7bd
jose <jose@gmail.com>
2017-10-19 16:50:36 +0000

    tots

--- modules/ejecutarActualizarModule.sh
+++ modules/ejecutarActualizarModule.sh
@@ -2,4 +2,4 @@
 #Ejecuta actualizando el modulo puesto a continuacion.
 #Igual que tambien tenemos que poner la empresa a ejecutar
 
-odoo --config /var/lib/odoo/.odoo_serverrc -d jose -u modul2 --save
+odoo --config /var/lib/odoo/.odoo_serverrc -d jose -u tots --save
